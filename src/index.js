8b62fbed3a8a610f3ee6b5735a51d3d1a600d710
Victor Quiroz Castro <victorhqc@gmail.com>
2019-02-04 15:13:38 +0100

    Add more mutations - post - updateLink - deleteLink

--- src/index.js
+++ src/index.js
@@ -1,19 +1,80 @@
 const { GraphQLServer } = require('graphql-yoga');
 
-const typeDefs = `
-type Query {
-  info: String!
-}
-`;
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL',
+}];
+let idCount = links.length;
+
+const getLinkById = (links, id) => {
+  const link = links.filter(link => link.id === id);
+
+  if (link.length === 0) {
+    return null;
+  }
+
+  return link[0];
+};
 
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-  }
+    feed: () => links,
+    link: (parent, args) => getLinkById(links, args.id),
+  },
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+
+      links.push(link);
+      return link;
+    },
+    updateLink: (parent, args) => {
+      const link = getLinkById(links, args.id);
+
+      if (!link) {
+        return null;
+      }
+
+      const updatedLink = {
+        ...link,
+        ...args,
+      };
+
+      links = links.map((link) => {
+        if (link.id !== args.id) { return link; }
+
+        return updatedLink;
+      });
+
+      return updatedLink;
+    },
+    deleteLink: (parent, args) => {
+      const link = getLinkById(links, args.id);
+
+      if (!link) {
+        return null;
+      }
+
+      links = links.filter(link => link.id !== args.id);
+
+      return link;
+    },
+  },
+  // Link: {
+  //   id: parent => parent.id,
+  //   description: parent => parent.description,
+  //   url: parent => parent.url,
+  // }
 };
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers,
 });
 
