b4f590213596f35b1ffe23b1ffa1be4f733693b2
Victor Quiroz Castro <victorhqc@gmail.com>
2019-02-05 16:53:42 +0100

    Add Subscription for new Lists

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ const Mutation = require('./resolvers/Mutation');
 const Query = require('./resolvers/Query');
 const User = require('./resolvers/User');
 const Link = require('./resolvers/Link');
+const Subscription = require('./resolvers/Subscription');
 
 const resolvers = {
   Query,
   Mutation,
+  Subscription,
   User,
   Link,
 };
