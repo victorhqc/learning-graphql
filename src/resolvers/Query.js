3dc41b83589b58fc147c4e4ba3f95fc51d62bf6b
Victor Quiroz Castro <victorhqc@gmail.com>
2019-02-06 10:28:07 +0100

    Add orderBy and count to response

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,4 +1,4 @@
-function feed(parent, args, context, info) {
+async function feed(parent, args, context, info) {
   const where = args.filter ? {
     OR: [
       { description_contains: args.filter },
@@ -6,12 +6,25 @@ function feed(parent, args, context, info) {
     ],
   } : {};
 
-  return context.prisma.links({
+  const links = context.prisma.links({
     where,
     skip: args.skip,
     first: args.first,
     last: args.last,
+    orderBy: args.orderBy,
   });
+
+  const count = context.prisma
+    .linksConnection({ where })
+    .aggregate()
+    .count();
+
+  // Fetch links & count in parallel. As normal await syntax would do it in sequence. This makes it
+  // a bit faster.
+  return {
+    links: await links,
+    count: await count,
+  };
 }
 
 module.exports = {
